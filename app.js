68b4cbd493b1da9546a79508cc434dec958a4b90
kiyong Nam <moosin76@gmail.com>
2025-09-21 17:55:51 +0900

    koa auto router

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const Koa = require('koa');
 const { koaBody } = require('koa-body');
 
-const Router = require('@koa/router');
-const router = new Router();
-
 const app = new Koa();
-
 app.use(koaBody()); // 코아 바디 파서
 
-const userRouter = require('./router/user');
-router.use('/user', userRouter.routes()); // 유저 라우터 prefix
-const testRouter = require('./router/test');
-router.use('/test', testRouter.routes());
-app.use(router.routes()).use(router.allowedMethods())
+const KoaAutoRouter = require('./KoaAutoRouter');
+KoaAutoRouter(app, '/router', "");
 
 app.listen(3000);
