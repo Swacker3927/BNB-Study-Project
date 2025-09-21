1ab08e22ec70021bc5bcf7680bf35edbffbacc90
kiyong Nam <moosin76@gmail.com>
2025-09-21 17:05:33 +0900

    중간

--- app.js
+++ app.js
@@ -1,56 +1,17 @@
 const Koa = require('koa');
-const Router = require('@koa/router');
 const { koaBody } = require('koa-body');
+
+const Router = require('@koa/router');
+const router = new Router();
 
 const app = new Koa();
 
 app.use(koaBody()); // 코아 바디 파서
 
-const router = new Router();
-
-router.get('/user', (ctx, next) => {
-	// DB 에 연결해서 사용자 목록 가져와서 줘야 겠지만!
-	ctx.body = {
-		rows: '사용자 목록 배열!'
-	};
-});
-
-router.get('/user/:id', (ctx, next) => {
-	const id = ctx.params.id;
-	console.log("userID:", id)
-	ctx.body = {
-		row: '유저 정보',
-		id,
-	};
-});
-
-router.post('/user', (ctx, next) => {
-	const payload = ctx.request.body;
-	ctx.body = {
-		row: '유저 회원가입',
-		payload,
-	};
-});
-
-router.put('/user/:id', (ctx, next) => {
-	const id = ctx.params.id;
-	const payload = ctx.request.body;
-	ctx.body = {
-		row: '유저 정보 수정',
-		id,
-		payload,
-	};
-});
-
-router.delete('/user/:id', (ctx, next) => {
-	const id = ctx.params.id;
-	ctx.body = {
-		row: '유저 정보 삭제',
-		id,
-	};
-});
-
-
-app.use(router.routes()).use(router.allowedMethods());
+const userRouter = require('./router/user');
+router.use('/user', userRouter.routes()); // 유저 라우터 prefix
+const testRouter = require('./router/test');
+router.use('/test', testRouter.routes());
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000);
